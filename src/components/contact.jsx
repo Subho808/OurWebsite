bfb68020b06a83f8303f31d3a9a75e927dcd6b5c
Subho808 <subhojitsaha808@gmail.com>
2024-08-27 17:30:44 +0530

    footer change

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -172,7 +172,7 @@ export const Contact = (props) => {
                 src={`/img/portfolio/logo (1).png`}
                 alt="Logo"
               />TOSTRING Company. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
+            <a href="https://ourwebsite-5e29c.web.app/" rel="nofollow">
               TOSTRING
             </a>
           </p>
